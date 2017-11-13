e505cab869b6a86165c3631c58078a7159fe0101
Veronika Andersen <veronika@arkitektum.no>
2017-11-13 13:07:24 +0100

    Translate breadcrumb and error page, and add english footer

--- Geonorge.Symbol/Global.asax.cs
+++ Geonorge.Symbol/Global.asax.cs
@@ -3,14 +3,18 @@ using Geonorge.Symbol.App_Start;
 using log4net;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Security.Claims;
+using System.Threading;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Geonorge.Symbol.Models.Translations;
 
 namespace Geonorge.Symbol
 {
@@ -37,6 +41,23 @@ namespace Geonorge.Symbol
             Exception ex = Server.GetLastError().GetBaseException();
 
             log.Error("App_Error", ex);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            var cookie = Context.Request.Cookies["_culture"];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("_culture", Culture.NorwegianCode);
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+
+            if (!string.IsNullOrEmpty(cookie.Value))
+            {
+                var culture = new CultureInfo(cookie.Value);
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+            }
         }
     }
 }
